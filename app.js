eba2d2f3a3cfbcad36e7e0ea9db86e94290b70e7
aftabalam999 <course.aftabalam@gmail.com>
2026-01-20 12:11:09 +0530

    UI updated and reset button added

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const formElem = {
 const outputElem = {
   tipPerPersonVal: document.querySelector("#tip-per-person"),
   totalPerPersonVal: document.querySelector("#total-per-person"),
+  resetBtn: document.querySelector("#resetBtn"),
 };
 
 let p = document.createElement("P");
 p.textContent = "Enter a positive number";
 p.classList.add("wrong-input");
 
-formElem.submitBtn.addEventListener("click", (e) => {
-  
+formElem.submitBtn.addEventListener("click", () => {
   if (
     formElem.totalAmount.value > 0 &&
     formElem.tipPercent.value > 0 &&
@@ -26,27 +26,33 @@ formElem.submitBtn.addEventListener("click", (e) => {
       Number(formElem.tipPercent.value),
       Number(formElem.totalPerson.value),
     );
-  }else{
+  } else {
     showError();
     return;
   }
 });
+outputElem.resetBtn.addEventListener("click", () => {
+  formElem.totalAmount.value = "";
+  formElem.tipPercent.value = "";
+  formElem.totalPerson.value = "";
+  outputElem.tipPerPersonVal.textContent = `₹ 0`;
+  outputElem.totalPerPersonVal.textContent = `₹ 0`;
+});
+
 const calculateTip = (total, tip, person) => {
   const tipPerPerson = ((total * tip) / 100 / person).toFixed(2);
   const totalPerPerson = (total / person + Number(tipPerPerson)).toFixed(2);
-
   showResult(tipPerPerson, totalPerPerson);
-
 };
 const showResult = (tipPerPerson, totalPerPerson) => {
-  outputElem.tipPerPersonVal.textContent = `₹${tipPerPerson}`;
-  outputElem.totalPerPersonVal.textContent = `₹${totalPerPerson}`;
+  outputElem.tipPerPersonVal.textContent = `₹ ${tipPerPerson}`;
+  outputElem.totalPerPersonVal.textContent = `₹ ${totalPerPerson}`;
 };
 
-const showError = ()=>{
-    p.classList.remove("hidden");
-    formElem.totalPerson.after(p);
-    setTimeout(() => {
-      p.classList.add("hidden");
-    }, 2000);
-}
+const showError = () => {
+  p.classList.remove("hidden");
+  formElem.totalPerson.after(p);
+  setTimeout(() => {
+    p.classList.add("hidden");
+  }, 2000);
+};
